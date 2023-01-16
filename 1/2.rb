709633102c9bf5d2bd2b0aa6921b3785b657a840
RNrd00 <117254446+RNrd00@users.noreply.github.com>
2023-01-16 00:06:48 +0000

    [Update]Ruby

--- 1/2.rb
+++ 1/2.rb
@@ -486,4 +486,37 @@ dimensions.each do |length,width|
  end
  #page146
  
- 
+ dimensions=[
+     [10,20],
+     [30,40],
+     [50,60],
+     ]
+     
+dimensions.each_with_index do |length, width, i|
+    puts "length: #{length}, width: #{width}, i: #{i}"
+end
+
+ dimensions=[
+     [10,20],
+     [30,40],
+     [50,60],
+     ]
+     
+dimensions.each_with_index do |dimension, i|
+    length = dimension[0]
+    width = dimension[1]
+    puts "length: #{length}, width: #{width}, i: #{i}"
+end
+
+ dimensions=[
+     [10,20],
+     [30,40],
+     [50,60],
+     ]
+
+dimensions.each_with_index do |(length, width), i|
+    puts "length: #{length}, width: #{width}, i: #{i}"
+end
+#page147
+
+
