ea5ffb6b9e71908000d5158dd337d69011372a39
RNrd00 <117254446+RNrd00@users.noreply.github.com>
2023-01-20 01:46:24 +0000

    [Update]Ruby

--- 1/2.rb
+++ 1/2.rb
@@ -658,8 +658,230 @@ p a
 a = []
 while a.size < 5 do a << 1 end
 p a
-#155page
 
 a = []
 a << 1 while a.size < 5
 p a
+#155page
+
+a=[]
+while false
+    a << 1
+end
+p a
+
+begin
+    a << 1
+end while false
+p a
+
+a=[10,20,30,40,50]
+until a.size <= 3
+a.delete_at(-1)
+end
+p a
+
+numbers = [1,2,3,4]
+sum=0
+for n in numbers
+    sum+=n
+end
+p sum
+
+sum = 0
+for n in numbers do sum += n end
+p sum
+
+numbers = [1,2,3,4]
+sum = 0
+for n in numbers
+    sum_value = n.even? ? n*10 : n
+    sum += sum_value
+end
+p n
+p sum_value
+
+#numbers = [1,2,3,4,5]
+#loop do
+#    n=numbers.sample
+#    puts n
+#    break if n ==5
+#end
+
+#while true
+#    n = numbers.sample
+#    puts n
+#    break if n ==5
+#end
+
+def factorial(n)
+    n > 0 ? n*factorial(n-1) : 1
+end
+p factorial(5)
+p factorial(0)
+
+def factorial(n)
+    ret=1
+    (1..n).each  { |n| ret *= n }
+    ret
+end
+p factorial(5)
+p factorial(0)
+
+p (1..4).map { |n| n*10 }
+p 1.upto(5).select { |n| n.odd? }
+
+numbers = [1,2,3,4,5].shuffle
+numbers.each do |n|
+    puts n
+    break if n == 5
+end
+
+numbers = [1,2,3,4,5].shuffle
+i = 0
+while i < numbers.size
+    n=numbers[i]
+    puts n
+    break if n == 5
+    i +=1
+end
+#160page
+
+ret =
+while true
+    break
+end
+p ret
+
+ret =
+while true
+    break 123
+end
+p ret
+
+#fruits = ['apple', 'melon', 'orange']
+#numbers = [1,2,3]
+#fruits.each do |fruit|
+#    numbers.shuffle.each do |n|
+#        puts "#{fruit}, #{n}"
+#        break if n == 3
+#    end
+#end
+
+#fruits = ['apple','melon','orange']
+#numbers = [1,2,3]
+#catch :done do
+#    fruits.shuffle.each do |fruit|
+#        numbers.shuffle.each do |n|
+#        puts "#{fruit}, #{n}"
+#        if fruit == 'orange' && n == 3
+#            throw :done
+#        end
+#    end
+#end
+#end
+
+ret =
+catch :done do
+    throw :done
+end
+p ret
+
+ret =
+catch :done do
+    throw :done, 123
+end
+p ret
+
+def calc_with_break
+    numbers = [1,2,3,4,5,6]
+    target=nil
+    numbers.shuffle.each do |n|
+        target=n
+    break if n.even?
+end
+target*10
+end
+p calc_with_break
+
+def calc_with_return
+    numbers=[1,2,3,4,5,6]
+    target=nil
+    numbers.shuffle.each do |n|
+        target=n
+        return if n.even?
+    end
+    target*10
+end
+p calc_with_return
+#page 163
+
+#[1,2,3].each do |n|
+#    puts n
+#    return
+#end
+
+numbers = [1,2,3,4,5]
+numbers.each do |n|
+    next if n.even?
+    puts n
+end
+
+numbers =[1,2,3,4,5]
+i=0
+while i < numbers.size
+    n=numbers[i]
+    i+=1
+    next if n.even?
+    puts n
+end
+#164page
+
+fruits = ['apple','melon','orange']
+numbers = [1,2,3,4]
+fruits.each do |fruit|
+    numbers.each do |n|
+        next if n.even?
+        puts "#{fruit},#{n}"
+    end
+end
+
+#foods =['ピーマン','トマト','セロリ']
+#foods.each do |food|
+#    print "#{food}は好きですか？ =>"
+#    answer = ['はい','いいえ'].sample
+#    puts answer
+#    
+#    redo unless answer == 'はい'
+#end
+
+foods = ['ピーマン','トマト','セロリ']
+count = 0
+foods.each do |food|
+    print "#{food}は好きですか? =>"
+    answer = 'いいえ'
+    puts answer
+    
+    count += 1
+    redo if answer != 'はい' && count < 2
+    count = 0
+end
+#page169
+
+p {}.class
+
+a={
+    'japan' => 'yen',
+    'us' => 'dollar',
+    'india' => 'rupee'
+}
+p a
+a={
+    'japan' => 'yen',
+    'us' => 'dollar',
+    'india' => 'rupee',
+}
+p a
+#a={ 'japan' => 'yen', 'japan' => '円' }
+#p a
+
