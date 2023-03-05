34f0650ed64891d507725cdd7d096049e3056f2f
RNrd00 <117254446+RNrd00@users.noreply.github.com>
2023-03-05 08:20:25 +0000

    g

--- 1/2.rb
+++ 1/2.rb
@@ -1057,3 +1057,14 @@ h = { us:'dollar',india:'rupee'}
 p ({japan: 'yen'}).merge(h)
 #188page
 
+def buy_burger(menu,drink:true,potato:true,**others)
+    puts others
+end
+p buy_burger('fish',drink:true,potato:false,salad:true,chicken:false)
+
+def buy_burger(menu,options={})
+    puts options
+end
+p buy_burger 'fish','drink'=>true,'potato'=>false
+
+#page192
